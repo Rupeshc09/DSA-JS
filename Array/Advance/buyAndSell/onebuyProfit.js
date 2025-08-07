6e92aae8e27d6c25271803bfcf80fbb25e474275
Student-apna-college <rupeshchougule40@gmail.com>
2025-08-07 13:17:59 +0530

    Buy and sell

---
+++ Array/Advance/buyAndSell/onebuyProfit.js
@@ -0,0 +1,17 @@
+
+
+ let prices = [7,1,5,3,6,4]
+
+  function buys(prices){
+    let min=prices[0]
+    let max=0
+      for(let i=0;i<prices.length;i++){
+        if(prices[i]<min){
+            min=prices[i]
+        }else if(prices[i]-min>max){
+            max=prices[i]-min
+        }
+      }
+      return max
+  }
+console.log(buys(prices));
